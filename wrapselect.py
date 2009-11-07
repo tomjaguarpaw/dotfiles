ce2aaec45aea40025ed1eda4098f54193e820368
tom-darcs <tom-darcs@jaguarpaw.co.uk>
2009-11-07 16:35:25 +0000

    Change mutt geometry to 24 lines so three fit vertically

--- wrapselect.py
+++ wrapselect.py
@@ -4,7 +4,7 @@ wrap_dir = '/home/tom/Config/Desktop/wrap'
 
 d = {'todo': '%s/rxvt -cr red -e /home/tom/bin/shortcut/todo',
      'fetchmail': '%s/fetchmail',
-     'mutt': 'EDITOR=jmacs xterm -bg black -fg grey -geometry 104x37 -e %s/mutt',
+     'mutt': 'EDITOR=jmacs xterm -bg black -fg grey -geometry 104x24 -e %s/mutt',
      'rxvt': '%s/rxvt',
      'rxvt-screen': '%s/rxvt-screen'
      }
