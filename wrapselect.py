15e39bdaaa8bb3e99f6b489c5bde2452eb4db44f
tom-darcs <tom-darcs@jaguarpaw.co.uk>
2010-02-22 17:39:40 +0000

    Include the todo wrapper in the repo

--- wrapselect.py
+++ wrapselect.py
@@ -2,7 +2,8 @@ import os, sys
 
 wrap_dir = '/home/tom/Config/Desktop/wrap'
 
-d = {'todo': '%s/rxvt -cr red -e /home/tom/bin/shortcut/todo',
+d = {'todo': '%%s/rxvt -cr red -e sh %s/todo' % wrap_dir,
+     # ^^ This is a fairly shit way of doing substitution
      'fetchmail': '%s/fetchmail',
      'mutt': 'EDITOR=jmacs xterm -bg black -fg grey -geometry 104x24 -e %s/mutt',
      'rxvt': '%s/rxvt',
