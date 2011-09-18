e2725ecba7e3d02a6a909dd44d84606d7a1cc723
tom-darcs <tom-darcs@jaguarpaw.co.uk>
2011-09-18 10:26:06 +0000

    Use sh to execute scripts, and remove geometry from rxvt

--- wrapselect.py
+++ wrapselect.py
@@ -2,12 +2,12 @@ import os, sys
 
 wrap_dir = '/home/tom/Config/Desktop/wrap'
 
-d = {'todo': '%%s/rxvt -cr red -e sh %s/todo' % wrap_dir,
+d = {'todo': 'sh %%s/rxvt -cr red -e sh %s/todo' % wrap_dir,
      # ^^ This is a fairly shit way of doing substitution
-     'fetchmail': '%s/fetchmail',
+     'fetchmail': 'sh %s/fetchmail',
      'mutt': 'EDITOR=jmacs xterm -bg black -fg grey -geometry 104x24 -e %s/mutt',
-     'rxvt': '%s/rxvt',
-     'rxvt-screen': '%s/rxvt-screen'
+     'rxvt': 'sh %s/rxvt',
+     'rxvt-screen': 'sh %s/rxvt-screen'
      }
 
 for command in sys.argv[1:]:
