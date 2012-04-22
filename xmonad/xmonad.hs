bf8ad7b3ddb198b280416ced6ce319cc9549f485
tom-darcs <tom-darcs@jaguarpaw.co.uk>
2012-04-22 06:34:04 +0000

    Add xmobar

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,8 +1,20 @@
+import System.IO(hPutStrLn)
 import XMonad
 import XMonad.Util.EZConfig(additionalKeys, removeKeys)
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
 
 main = do
-    xmonad $ defaultConfig `additionalKeys` [((0, xK_F1), spawn "python /home/tom/Config/Desktop/wrapselect.py rxvt-screen")
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ defaultConfig { manageHook = manageDocks <+> manageHook defaultConfig
+                           , layoutHook = avoidStruts  $  layoutHook defaultConfig
+                           , logHook = dynamicLogWithPP xmobarPP
+                                       { ppOutput = hPutStrLn xmproc
+                                       , ppTitle = xmobarColor "green" "" . shorten 50
+                                       }
+                           }
+    	     		   `additionalKeys` [((0, xK_F1), spawn "python /home/tom/Config/Desktop/wrapselect.py rxvt-screen")
                                             ,((0, xK_F2), spawn "python /home/tom/Config/Desktop/wrapselect.py todo")
                                             ,((mod1Mask .|. shiftMask, xK_q), spawn "ls") -- how do I actually remove keys?  `removeKeys` doesn't seem to work
                                             ,((mod1Mask, xK_q), spawn "ls")
