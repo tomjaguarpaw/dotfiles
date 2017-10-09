d7fc1eedeb0c255d5a6545b260d12766d021558c
Tom Ellis <tom-2013@purelyagile.com>
2017-10-09 23:48:34 +0100

    Update to newer XMonad
    * defaultConfig -> def
    
    * Remove tabs

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -16,16 +16,16 @@ tomppLayout s = s
 
 main = do
     xmproc <- spawnPipe "xmobar"
-    xmonad $ defaultConfig { manageHook = manageDocks <+> manageHook defaultConfig
-                           , layoutHook = smartBorders $ avoidStruts $ layoutHook defaultConfig
+    xmonad $ def { manageHook = manageDocks <+> manageHook def
+                           , layoutHook = smartBorders $ avoidStruts $ layoutHook def
                            , logHook = dynamicLogWithPP xmobarPP
                                        { ppOutput = hPutStrLn xmproc
                                        , ppTitle = xmobarColor "green" "" . shorten 100
-				       , ppLayout = xmobarColor "lightblue" "" . tomppLayout
+                                       , ppLayout = xmobarColor "lightblue" "" . tomppLayout
                                        }
                            , borderWidth = 2
                            }
-    	     		   `additionalKeys` [((0, xK_F1), wrapSelect "rxvt-tmux")
+                           `additionalKeys` [((0, xK_F1), wrapSelect "rxvt-tmux")
                                             ,((0, xK_F2), wrapSelect "todo")
                                             ,((mod1Mask .|. shiftMask, xK_q), spawn "ls") -- how do I actually remove keys?  `removeKeys` doesn't seem to work
                                             ,((mod1Mask, xK_q), spawn "ls")
