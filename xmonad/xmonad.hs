b9b672464b0ca12c0f178e61f692d7f584d17ef2
Tom Ellis <tom-2013@purelyagile.com>
2017-10-14 09:01:33 +0100

    wsp

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -17,17 +17,17 @@ tomppLayout s = s
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad $ def { manageHook = manageDocks <+> manageHook def
-                           , layoutHook = smartBorders $ avoidStruts $ layoutHook def
-                           , logHook = dynamicLogWithPP xmobarPP
-                                       { ppOutput = hPutStrLn xmproc
-                                       , ppTitle = xmobarColor "green" "" . shorten 100
-                                       , ppLayout = xmobarColor "lightblue" "" . tomppLayout
-                                       }
-                           , borderWidth = 2
-                           }
-                           `additionalKeys` [((0, xK_F1), wrapSelect "rxvt-tmux")
-                                            ,((0, xK_F2), wrapSelect "todo")
-                                            ,((mod1Mask .|. shiftMask, xK_q), spawn "ls") -- how do I actually remove keys?  `removeKeys` doesn't seem to work
-                                            ,((mod1Mask, xK_q), spawn "ls")
-                                            ]
-                           `removeKeys` [(mod1Mask, xK_w), (mod1Mask, xK_q), (mod1Mask .|. shiftMask, xK_q)]
+                 , layoutHook = smartBorders $ avoidStruts $ layoutHook def
+                 , logHook = dynamicLogWithPP xmobarPP
+                               { ppOutput = hPutStrLn xmproc
+                               , ppTitle = xmobarColor "green" "" . shorten 100
+                               , ppLayout = xmobarColor "lightblue" "" . tomppLayout
+                               }
+                 , borderWidth = 2
+                 }
+              `additionalKeys` [((0, xK_F1), wrapSelect "rxvt-tmux")
+                               ,((0, xK_F2), wrapSelect "todo")
+                               ,((mod1Mask .|. shiftMask, xK_q), spawn "ls") -- how do I actually remove keys?  `removeKeys` doesn't seem to work
+                               ,((mod1Mask, xK_q), spawn "ls")
+                               ]
+              `removeKeys` [(mod1Mask, xK_w), (mod1Mask, xK_q), (mod1Mask .|. shiftMask, xK_q)]
