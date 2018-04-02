cbb018e7eb2e8ac10569b76b8fadb3bebf85b3ba
Tom Ellis <tom-2013@purelyagile.com>
2018-04-02 11:53:42 +0100

    Terminate if we are not given a readable directory

--- setup.sh
+++ setup.sh
@@ -2,6 +2,13 @@ set -e
 
 DOTFILES=$1
 
+if [ ! -r "$DOTFILES" ]; then
+    echo I could not read the directory "$DOTFILES"
+    echo
+    echo Usage: sh setup.sh '<path to dotfiles directory>'
+    exit
+fi
+
 ln -s $DOTFILES/terminfo ~/.terminfo
 ln -s $DOTFILES/screenrc ~/.screenrc
 ln -s $DOTFILES/tmux.conf ~/.tmux.conf
