89e2d728332e72491f856e83c092df94a8a6904f
Tom Ellis <tom-git@jaguarpaw.co.uk>
2019-07-18 20:45:53 +0100

    Rename emacsdir to Emacs

--- setup.sh
+++ setup.sh
@@ -15,3 +15,4 @@ ln -s $DOTFILES/terminfo ~/.terminfo
 ln -s $DOTFILES/screenrc ~/.screenrc
 ln -s $DOTFILES/tmux.conf ~/.tmux.conf
 ln -s $DOTFILES/zsh/zshrc ~/.zshrc
+ln -s $DOTFILES/Emacs/emacs ~/.emacs
