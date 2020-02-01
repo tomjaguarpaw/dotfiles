e3308d4d14b56315279fdd48b05aa847c856c89c
Tom Ellis <tom-git@jaguarpaw.co.uk>
2020-02-01 14:35:24 +0000

    Add hook for emacs latex mode

--- Emacs/latex.el
+++ Emacs/latex.el
@@ -4,4 +4,9 @@
 	(font-lock-mode)
 )
 
+; latex-mode-hook is for the emacs builtin latex mode.
+; LaTeX-mode-hook is for AUCTeX's latex mode.
+;
+;     https://emacs.stackexchange.com/a/7469
 (add-hook 'LaTeX-mode-hook 'my-latex-mode-hook)
+(add-hook 'latex-mode-hook 'my-latex-mode-hook)
