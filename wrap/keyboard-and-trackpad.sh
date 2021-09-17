63667b62c45342e1b10caf9daf5e88a4c301430c
Tom <tom@debian-BULLSEYE-live-builder-AMD64>
2021-09-17 18:59:23 +0100

    Set up keyboard and trackpad

---
+++ wrap/keyboard-and-trackpad.sh
@@ -0,0 +1,5 @@
+setxkbmap dvorak
+xmodmap /home/tom/Config/dotfiles/xmodmaps/caps
+
+xinput --set-prop "CUST0001:00 06CB:76B1 Touchpad" "libinput Natural Scrolling Enabled" 1
+xinput --set-prop "CUST0001:00 06CB:76B1 Touchpad" "libinput Tapping Enabled" 1
