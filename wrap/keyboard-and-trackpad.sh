73a63f2cf882b5183e3e070fa16a3ee10dc0dd75
Tom Ellis <tom-git@jaguarpaw.co.uk>
2021-12-14 12:26:00 +0000

    Add Varmilo keyboard config

--- wrap/keyboard-and-trackpad.sh
+++ wrap/keyboard-and-trackpad.sh
@@ -5,3 +5,8 @@ xinput --set-prop "CUST0001:00 06CB:76B1 Touchpad" "libinput Natural Scrolling E
 xinput --set-prop "CUST0001:00 06CB:76B1 Touchpad" "libinput Tapping Enabled" 1
 xinput --set-prop "Logitech USB Optical Mouse" "libinput Natural Scrolling Enabled" 1
 xinput --set-prop "Logitech USB Optical Mouse" "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 1.7
+
+# https://wiki.archlinux.org/title/Apple_Keyboard#Function_keys_do_not_work
+# if [ -e /sys/module/hid_apple/parameters/fnmode ]; then
+#     sudo sh -c "echo 2 > /sys/module/hid_apple/parameters/fnmode"
+# fi
