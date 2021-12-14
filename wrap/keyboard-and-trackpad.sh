a98489d6c6c5e8b6c4235efdd7b10fc728664bc8
Tom Ellis <tom-git@jaguarpaw.co.uk>
2021-12-14 12:25:45 +0000

    Add mouse config

--- wrap/keyboard-and-trackpad.sh
+++ wrap/keyboard-and-trackpad.sh
@@ -3,3 +3,5 @@ xmodmap /home/tom/Config/dotfiles/xmodmaps/caps
 
 xinput --set-prop "CUST0001:00 06CB:76B1 Touchpad" "libinput Natural Scrolling Enabled" 1
 xinput --set-prop "CUST0001:00 06CB:76B1 Touchpad" "libinput Tapping Enabled" 1
+xinput --set-prop "Logitech USB Optical Mouse" "libinput Natural Scrolling Enabled" 1
+xinput --set-prop "Logitech USB Optical Mouse" "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 1.7
