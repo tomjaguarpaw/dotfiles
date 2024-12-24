671891943321a67e094067656a9cc2b50ef5206c
Tom Ellis <tom-git@jaguarpaw.co.uk>
2024-12-24 16:19:11 +0000

    New vertical mouse

--- wrap/keyboard-and-trackpad.sh
+++ wrap/keyboard-and-trackpad.sh
@@ -3,8 +3,12 @@ xmodmap /home/tom/Config/dotfiles/xmodmaps/caps
 
 xinput --set-prop "CUST0001:00 06CB:76B1 Touchpad" "libinput Natural Scrolling Enabled" 1
 xinput --set-prop "CUST0001:00 06CB:76B1 Touchpad" "libinput Tapping Enabled" 1
-xinput --set-prop "Logitech USB Optical Mouse" "libinput Natural Scrolling Enabled" 1
-xinput --set-prop "Logitech USB Optical Mouse" "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 1.7
+# New vertical mouse
+xinput --set-prop "USB Optical Mouse" "libinput Natural Scrolling Enabled" 1
+xinput --set-prop "USB Optical Mouse" "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 1.7
+# Old mouse:
+# xinput --set-prop "Logitech USB Optical Mouse" "libinput Natural Scrolling Enabled" 1
+# xinput --set-prop "Logitech USB Optical Mouse" "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 1.7
 
 FNMODE=/sys/module/hid_apple/parameters/fnmode
 
